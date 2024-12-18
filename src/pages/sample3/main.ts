f6baf3a37497ed3625847c639f2685cb8697691f
Soma Sekine <sekine.soma@junni.jp>
2024-12-18 18:58:43 +0900

    遠近感でてた！

--- src/pages/sample3/main.ts
+++ src/pages/sample3/main.ts
@@ -5,8 +5,10 @@ import fragmentShader from "./shader/fragment.glsl?raw";
 
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
-const SIZE = 3000;
+const SIZE = 30;
 const LENGTH = 10000;
+
+const scales = new Float32Array(LENGTH * 1);
 
 const vertices: number[] = [];
 
@@ -19,6 +21,8 @@ for (let i = 0; i < LENGTH; i++) {
     const z = SIZE * Math.cos(phi);
 
     vertices.push(x, y, z);
+
+    scales[i] = Math.random();
 }
 
 const geometry = new THREE.BufferGeometry();
@@ -26,10 +30,32 @@ geometry.setAttribute(
     "position",
     new THREE.Float32BufferAttribute(vertices, 3)
 );
+geometry.setAttribute("aScale", new THREE.BufferAttribute(scales, 1));
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const material = new THREE.ShaderMaterial({
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
+    uniforms: {
+        uSize: { value: 30 * renderer.getPixelRatio() },
+        uTime: { value: 0 },
+    }
 });
 
 const scene = new THREE.Scene();
@@ -38,11 +64,6 @@ scene.background = new THREE.Color(0x111111);
 
 const mesh = new THREE.Points(geometry, material);
 scene.add(mesh);
-
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-};
 
 window.addEventListener("resize", () => {
     // Update sizes
@@ -66,25 +87,16 @@ const camera = new THREE.PerspectiveCamera(
     75,
     sizes.width / sizes.height,
     0.1,
-    50000
+    100
 );
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 3000;
+camera.position.z = 3;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
@@ -93,6 +105,8 @@ const clock = new THREE.Clock();
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+
+    material.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update();
